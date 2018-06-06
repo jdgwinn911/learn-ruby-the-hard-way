7d211f8d2fe6c9fd13db17a368b8fba3afbd14ec
joshua Gwinn <jdgwinn911@gmail.com>
2018-06-06 11:21:47 -0400

    adding exercise 5

---
+++ ex5.rb
@@ -0,0 +1,17 @@
+my_name = 'Joshua (Demogorgon) D. Gwinn'
+my_age = 26 # born in 1991 july 25th.
+my_height = 65 # inches
+my_weight = 190 #lbs
+my_eyes = 'black'
+my_teeth = 'ugly,yellow,and overall unplesant'
+my_hair = 'brownis black'
+
+puts "let's talk about #{my_name}."
+puts "He's #{my_height} inches tall."
+puts "He's #{my_weight} pounds heavy."
+puts "Actually thats not too heavy."
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} but on occasion he brushes them."
+
+#this line is trcky (not really), try to get it exactly right.
+puts " If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
