f73e9efad56f1f9fcc1b53fb5fd45cd6b2a2ef3f
joshua Gwinn <jdgwinn911@gmail.com>
2018-06-06 14:07:26 -0400

    adding exercise 9 to the learn ruby the hard way repo

---
+++ ex9.rb
@@ -0,0 +1,14 @@
+#Here's some new strange stuff, remember type it exactly.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+puts "Here are the days of the week: #{days}"
+puts "Here are the months of the year: #{months}"
+
+puts %q{
+    there's something going on here,
+    with this weird quote
+    we'll be able to type as much as we like,
+    even 4 lines if we want, or 5, or 6.
+}
